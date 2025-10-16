fe83016b07bbc108c61f737004c55ed8aeb606c3
João Souza <joao987656@gmail.com>
2025-10-16 16:27:58 +0100

    ManyToMany

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -17,7 +17,9 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imageUrl;
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "db_product_category",joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories=new HashSet<>();
 
     public Product() {
