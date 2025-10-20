f7ced8f80c9e87949de378431f474c3ac8bc74ac
João Souza <joao987656@gmail.com>
2025-10-20 17:23:50 +0100

    subTotal e total methods

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
     public Payment(Long id, Instant moment,  Order order) {
         this.id = id;
